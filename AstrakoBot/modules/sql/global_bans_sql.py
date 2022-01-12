4707e0a538d4bac36ea94bcde8e99bf254eca868
itzzzyashu <83384892+itzzzzyashu@users.noreply.github.com>
2022-01-12 12:27:46 +0530

    Update global_bans_sql.py

--- AstrakoBot/modules/sql/global_bans_sql.py
+++ AstrakoBot/modules/sql/global_bans_sql.py
@@ -1,12 +1,12 @@
 import threading
 
 from AstrakoBot.modules.sql import BASE, SESSION
-from sqlalchemy import Boolean, Column, Integer, BigInteger, String, UnicodeText
+from sqlalchemy import Boolean, Column, Integer, BIGINT, String, UnicodeText
 
 
 class GloballyBannedUsers(BASE):
     __tablename__ = "gbans"
-    user_id = Column(BigInteger, primary_key=True)
+    user_id = Column(BIGINT, primary_key=True)
     name = Column(UnicodeText, nullable=False)
     reason = Column(UnicodeText)
 
